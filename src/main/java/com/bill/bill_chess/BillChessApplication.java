0de5cdd63e15d21f1baf0a3004c7b3839a71c74b
antoniocariocajr <antoniocariocajr@gmail.com>
2025-12-08 07:14:42 -0300

    first commit

---
+++ src/main/java/com/bill/bill_chess/BillChessApplication.java
@@ -0,0 +1,13 @@
+package com.bill.bill_chess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillChessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillChessApplication.class, args);
+	}
+
+}
